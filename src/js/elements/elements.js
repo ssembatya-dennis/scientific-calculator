3aa1bc1f80294d1096be3295ef9b0178b7df7a11
dennis25 <ssembatyadennis25@gmail.com>
2024-02-03 12:31:12 +0300

    feat(elements): add id(s) on individual buttons and select them with javascript.
    - add the elements module for purposely exporting different DOM elements.

---
+++ src/js/elements/elements.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Number Buttons
+const nineButtonEl = document.getElementById("nine");
+const eightButtonEl = document.getElementById("eight");
+const sevenButtonEl = document.getElementById("seven");
+const sixButtonEl = document.getElementById("six");
+const fiveButtonEl = document.getElementById("five");
+const fourButtonEl = document.getElementById("four");
+const threeButtonEl = document.getElementById("three");
+const twoButtonEl = document.getElementById("two");
+const oneButtonEl = document.getElementById("one");
+const zeroButtonEl = document.getElementById("zero");
+const pointButtonEl = document.getElementById("point");
+
+// Operator Buttons
+const divideButtonEl = document.getElementById("divide");
+const multiplyButtonEl = document.getElementById("multiply");
+const subtractButtonEl = document.getElementById("minus");
+const addButtonEl = document.getElementById("plus");
+const clearButtonEl = document.getElementById("clear");
+const equalButtonEl = document.getElementById("equal");
+
+// UI elements
+export const displayScreenEl = document.getElementById("display");
+
+export const numberArray = [
+  nineButtonEl,
+  eightButtonEl,
+  sevenButtonEl,
+  sixButtonEl,
+  fiveButtonEl,
+  fourButtonEl,
+  threeButtonEl,
+  twoButtonEl,
+  oneButtonEl,
+  zeroButtonEl,
+  pointButtonEl,
+];
+
+export const operatorButtonArray = [
+  divideButtonEl,
+  multiplyButtonEl,
+  subtractButtonEl,
+  addButtonEl,
+  clearButtonEl,
+  equalButtonEl,
+];
