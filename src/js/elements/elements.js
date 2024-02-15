45cc44d556fad1e22d7141c5a8ed1c662ecca11e
dennis25 <ssembatyadennis25@gmail.com>
2024-02-15 23:50:06 +0300

    feat(elements): select all the buttons in the elements file

---
+++ src/js/elements/elements.js
@@ -0,0 +1,43 @@
+/**
+ * File containing various elements in the DOM
+ */
+// Number Button elements
+const nineButtonEl = document.getElementById("nine");
+const eightButtonEl = document.getElementById("eight");
+const sevenButtonEl = document.getElementById("seven");
+const sixButtonEl = document.getElementById("six");
+const fiveButtonEl = document.getElementById("five");
+const fourButtonEl = document.getElementById("four");
+const threeButtonEl = document.getElementById("three");
+const twoButtonEl = document.getElementById("two");
+const oneButtonEl = document.getElementById("one");
+const pointButtonEl = document.getElementById("point");
+
+// Operator Button elements
+const plusButtonEl = document.getElementById("plus");
+const minusButtonEl = document.getElementById("minus");
+const multiplyButtonEl = document.getElementById("multiply");
+const divideButtonEl = document.getElementById("divide");
+const equalButtonEl = document.getElementById("equal");
+const clearButtonEl = document.getElementById("clear");
+const deleteButtonEl = document.getElementById("delete");
+
+export {
+  nineButtonEl,
+  eightButtonEl,
+  sevenButtonEl,
+  sixButtonEl,
+  fiveButtonEl,
+  fourButtonEl,
+  threeButtonEl,
+  twoButtonEl,
+  oneButtonEl,
+  pointButtonEl,
+  plusButtonEl,
+  minusButtonEl,
+  multiplyButtonEl,
+  divideButtonEl,
+  equalButtonEl,
+  clearButtonEl,
+  deleteButtonEl,
+};
